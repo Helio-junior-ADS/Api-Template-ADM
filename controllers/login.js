b78ddea22dbd3c82c38f011052ed0c3a9e2b2862
Hélio Júnior <heliojunior@MacBook-Air-de-Helio.local>
2023-11-15 08:02:44 -0300

    carregar layout diferente com handlebars

--- controllers/login.js
+++ controllers/login.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req,res)=> {
-    return res.status(200).send("Página Login");
+    res.render("admin/login/login", {layout: 'login'});
 });
 
 module.exports = router;
